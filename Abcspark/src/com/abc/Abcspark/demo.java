3ce044f973ef3a85de8098a1c5a2528b96072eab
kumar <kumar@GOWDA>
2019-04-04 11:34:56 -0700

    change from chrome to ie

--- Abcspark/src/com/abc/Abcspark/demo.java
+++ Abcspark/src/com/abc/Abcspark/demo.java
@@ -3,11 +3,12 @@ package com.abc.Abcspark;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class demo {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new InternetExplorerDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com");
 		
